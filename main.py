a807e1c41bd0457e14b44f9d9fdd65f851e53942
Morinaka3104 <121215896+Morinaka3104@users.noreply.github.com>
2022-12-22 16:11:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import streamlit as st
+with open("1-1.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
 
-st.title('生保二次試験用')
-
+st.download_button(label="Download PDF Tutorial",
+        data=PDFbyte,
+        file_name="1-1.pdf",
+        mime='application/octet-stream')
